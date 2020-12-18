ce73eae4c3b18421327888a259add92e9613518c
Grant Murray <grant.murray@yum.com>
2020-12-18 15:57:15 -0600

    Connector tests stable again

--- snowmobile/core/connector/connector.py
+++ snowmobile/core/connector/connector.py
@@ -4,9 +4,12 @@ SnowflakeConnection for query/statement execution.
 """
 from __future__ import annotations
 from pathlib import Path
-from typing import Union
+from typing import Union, ContextManager
+from contextlib import contextmanager
 
 import pandas as pd
+from pandas.io.sql import DatabaseError
+
 from snowflake.connector import connect
 from snowflake.connector.connection import SnowflakeConnection, SnowflakeCursor
 from snowflake.connector.errors import ProgrammingError
@@ -34,6 +37,7 @@ class Connector:
         from_config: Union[str, Path] = None,
         ensure_alive: bool = True,
         delay: bool = False,
+        mode: str = None,
         **kwargs,
     ):
         """
@@ -52,13 +56,18 @@ class Connector:
                 with specific file-system configurations.
 
         """
+        self._error: bool = bool()
+
         self.cfg: Configuration = Configuration(
-            config_file_nm=config_file_nm, creds=creds, from_config=from_config
+            creds=creds,
+            config_file_nm=config_file_nm,
+            from_config=from_config
         )
         self.ensure_alive = ensure_alive
-        self._delayed = delay
+        # self.delayed = delay
         self.conn: SnowflakeConnection = None
         self.sql: sql.SQL = sql.SQL(sn=self)
+        self.mode = mode or 'e'
         if not delay:
             self.connect(**kwargs)
 
@@ -74,16 +83,15 @@ class Connector:
         try:
             self.conn = connect(
                 **{
-                    **self.cfg.connection.current.credentials,
-                    **self.cfg.connection.defaults,
-                    **kwargs
+                    **self.cfg.connection.current.credentials,  # credentials
+                    **self.cfg.connection.defaults,             # defaults
+                    **kwargs                                    # over-rides/additional
                 },
             )
-
-            self._delayed = False
+            # self.delayed = False
             self.sql = sql.SQL(sn=self)
 
-            print(self)
+            print(str(self))
             return self
 
         except ProgrammingError as e:
@@ -92,17 +100,22 @@ class Connector:
     def disconnect(self) -> Connector:
         """Disconnect from connection with which Connector() was instantiated."""
         self.conn.close()
-        self._delayed = True
+        # self.delayed = True
+        self.conn = None
         return self
 
     @property
     def alive(self) -> bool:
         """Check if the connection is still alive."""
-        return False if self._delayed else not self.cursor.is_closed()
+        if not isinstance(self.conn, SnowflakeConnection):
+            return False
+        return not self.cursor.is_closed()
 
     @property
-    def cursor(self) -> SnowflakeCursor:
+    def cursor(self) -> Union[SnowflakeCursor, None]:
         """:class:`SnowflakeCursor` accessor off :class:`snowmobile.Connect`."""
+        if not isinstance(self.conn, SnowflakeConnection):
+            self.connect()
         return self.conn.cursor()
 
     def ex(self, sql: str, **kwargs) -> SnowflakeCursor:
@@ -118,7 +131,11 @@ class Connector:
             :class:`SnowflakeCursor` object that executed the command.
 
         """
-        return self.cursor.execute(command=sql, **kwargs)
+        try:
+            return self.cursor.execute(command=sql, **kwargs)
+        except ProgrammingError as e:
+            self._error = e
+            raise ProgrammingError(f"ProgrammingError: {e}")
 
     def query(
         self, sql: str, results: bool = True, lower: bool = True,
@@ -139,16 +156,28 @@ class Connector:
             :class:`SnowflakeCursor` object if `Results=False`.
 
         """
-        if self._delayed or (self.ensure_alive and not self.alive):
-            self.connect()
+        if not results:
+            return self.ex(sql=sql)
+
         try:
-            if not results:
-                return self.ex(sql)
+            self._connect()
             df = pd.read_sql(sql, con=self.conn)
             return df.snowmobile.lower_cols() if lower else df
+        except DatabaseError as e:
+            self._error = e
+            raise e
 
-        except ProgrammingError as e:
-            raise ProgrammingError(f"ProgrammingError: {e}")
+    def _connect(self):
+        """Connects to db if not connected and a connection is implicitly invoked."""
+        if not self.alive and self.ensure_alive:
+            self.connect()
+        return self
+
+    def __setattr__(self, key, value):
+        vars(self)[key] = value
+
+    def __setitem__(self, key, value):
+        vars(self)[key] = value
 
     def __str__(self) -> str:
         return f"snowmobile.Connect(creds='{self.cfg.connection.creds}')"
