b408402211985a22860130f8781b89071faadb96
Grant Murray <grant.murray@yum.com>
2020-12-20 11:15:57 -0600

    Comment cleanup

--- snowmobile/core/pkg_data/.snowmobile/export_ddl.py
+++ snowmobile/core/pkg_data/.snowmobile/export_ddl.py
@@ -2,14 +2,13 @@
 This file exists for the purpose of exporting DDL.sql to a markdown file.
 
 The relative 'ddl_location' path below assumes that this script is stored
-within '/pkg_data/.snowmobile'.
+within 'snowmobile/core/pkg_data/.snowmobile' and that `DDL.sql` is in `pkg_data`.
 """
 from pathlib import Path
 import snowmobile
 
 # location of the DDL.sql file
 ddl_location = Path(__file__).absolute().parent.parent / "DDL.sql"
-# ddl_location = Path(r'C:\Users\GEM7318\Documents\Github\Snowmobile\snowmobile\core\pkg_data\DDL.sql')
 
 # connector object, connection omitted
 sn = snowmobile.Connect(delay=True, config_file_nm='snowmobile_testing.toml')
